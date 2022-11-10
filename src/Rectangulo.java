764f948f8cc1f4c2204889abcfe463c364434609
ej <alujip8100@ieselcaminas.org>
2022-11-10 15:30:54 +0100

    Version 4

---
+++ src/Rectangulo.java
@@ -0,0 +1,14 @@
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo (double lado1, double lado2){
+        lado1=1;
+        lado2=2;
+    }
+
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+
+}
